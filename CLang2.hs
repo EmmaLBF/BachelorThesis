abf5f8ca79913842b2e7eea47ea4529af3cdeb37
EmmaLBF <em.lou.bilz.fer@gmail.com>
2026-04-16 16:17:05 +0200

    cleaned code, works for fac 4,5 and 6

--- CLang2.hs
+++ CLang2.hs
@@ -15,17 +15,12 @@ import Control.Monad.State
 import Data.Typeable
 import Debug.Trace
 
-indentStr :: Int -> String
-indentStr n = replicate (2 * n) ' '
-
 data CValue a where
     IntV :: Int -> CValue Int
     BoolV :: Bool -> CValue Bool
     UnitV :: CValue ()
     FunV  :: (CValue a -> CValue b) -> CValue (a -> b)
     PairV :: CValue a -> CValue b -> CValue (a, b)
-
--- deriving instance Typeable a => Typeable (CValue a)
 
 data CExpression a where
     Var :: (Typeable a) => Int -> CExpression a
@@ -38,26 +33,10 @@ data CExpression a where
     Fst  :: (Typeable a, Typeable b) => CExpression (a, b) -> CExpression a
     Snd  :: (Typeable a, Typeable b) => CExpression (a, b) -> CExpression b
 
--- in theory statements shouldn't have a type? since they don't return anything?
--- data CStatement a where
---   Return :: Typeable a => CExpression a -> CStatement a
---   Bind :: Typeable a => CStatement a -> Int -> CStatement b -> CStatement b
---   Seq :: CStatement a -> CStatement b -> CStatement b
---   If :: CExpression Bool -> CStatement a -> CStatement a -> CStatement a
-
---   -- Do not return anything
---   DefFun :: (Typeable a, Typeable b) => Proxy a -> Int -> Int -> CStatement b -> CStatement ()
---   DefVar :: Typeable a => Int -> CExpression a -> CStatement ()
---   UpdateVar :: Typeable a => Int -> CExpression a -> CStatement ()
---   While :: CExpression Bool -> CStatement a -> CStatement ()
 data CStatement where
-  -- Return :: Typeable a => CExpression a -> CStatement
   BindExpr :: Typeable a => CExpression a -> Int -> CStatement -> CStatement
-  -- Bind :: CStatement -> Int -> CStatement -> CStatement
   Seq :: CStatement -> CStatement -> CStatement
   If :: CExpression Bool -> CStatement -> CStatement -> CStatement
-
-  -- Do not return anything
   DefFun :: (Typeable a, Typeable b) => Proxy a -> Int -> Int -> CStatement -> CExpression b -> CStatement
   DefVar :: Typeable a => Int -> CExpression a -> CStatement
   UpdateVar :: Typeable a => Int -> CExpression a -> CStatement
@@ -75,13 +54,8 @@ data Compiled a = Compiled
   , result :: CExpression a
   }
 
-type Trans a = State Int (Compiled a)
-
-onlyExpr :: CExpression a -> Trans a
+onlyExpr :: CExpression a -> State Int (Compiled a)
 onlyExpr expr = return $ Compiled { setup = Skip, result = expr }
-
--- toStatement :: Typeable a => Compiled a -> CStatement
--- toStatement c = Seq (setup c) (Return (result c))
 
 translate :: forall a. NL.NamedLang a -> State Int (Compiled a)
 translate (NL.LInt n) = onlyExpr (Val (IntV n))
@@ -118,8 +92,6 @@ translate (NL.If cond t f) = do
                    (If (result cc)
                        (Seq (setup ct) (UpdateVar n (result ct)))
                        (Seq (setup cf) (UpdateVar n (result cf))))
-      -- ret = Return (Var n `asTypeOf` result ct) -- should work since both branches have the same type?
-      -- stmt = Bind branch n ret
   return $ Compiled { setup = stmt, result = Var n }
 translate (NL.Lam arg i (f :: NL.NamedLang b)) = do
   cf <- translate f
@@ -128,74 +100,12 @@ translate (NL.Lam arg i (f :: NL.NamedLang b)) = do
   return $ Compiled { setup = stmt, result = Var ifun }
 translate (NL.Fix f) = do
   cf <- translate f
-  n <- fresh  -- this must not clash with any Lam parameter
+  n <- fresh
   let v = Var n :: CExpression a
       call = CallExpr (result cf) v
       bindStmt = DefVar n call
       stmt = Seq (setup cf) bindStmt
-      -- stmt = Seq  (Seq (setup cf) (Return call)) (Return (Val UnitV))
   return $ Compiled { setup = stmt, result = v }
--- translate (NL.Fix (NL.Lam i1 (NL.Lam i2 (NL.If cond base (NL.Apply (NL.Var i) arg))))) 
---   | i1 == i = do
---     ccond <- translate cond
---     cbase <- translate base
---     carg <- translate arg
---     acc <- fresh
---     n <- fresh
---     let v_acc = Var acc :: CExpression a
---         loop_body = Seq (UpdateVar i2 (result carg)) (UpdateVar acc (result cbase))
---         wh = Seq (UpdateVar acc (result cbase)) (While (Not (result ccond)) loop_body)
---         stmt = DefFun i2 (Seq (DefVar acc v_acc) wh)
---     return $ Compiled { setup = stmt, result = Var n }
--- translate (NL.Fix (NL.Lam iparam body)) = do
---   cf <- translate body
---   -- ifun <- fresh
---   -- iparam <- fresh
---   let stmt = DefFun iparam iparam (toStatement cf)
---   return $ Compiled { setup = stmt, result = Var iparam }
--- translate (NL.Fix f) = do
---   cf <- translate f
---   fixId <- fresh
---   argId <- fresh
---   fixVarId <- fresh  -- NEW: variable to hold the fixed-point result
-  
---   let selfVar = Var fixId :: CExpression (a -> a)
---       selfCall = CallExpr selfVar (Var argId) :: CExpression a
---       -- Replace the original function result with the self-call
---       recBody = cf { result = selfCall }
---       defStmt = DefFun (Proxy :: Proxy a) fixId argId (toStatement recBody)
---       bindStmt = Bind defStmt fixVarId (Return (Var fixVarId))
-  
---   return $ Compiled { setup = bindStmt, result = Var fixVarId }
-  -- case f of
-  --   (NL.Lam i _) -> do
-  --     cf <- translate f
-  -- -- iparam <- fresh
-  --     let v = Var i :: CExpression a
-  --         stmt = Bind (setup cf) i (Return v)
-  --     return $ Compiled { setup = stmt, result = Var i }
-  --   _ -> do
-  --     cf <- translate f
-  --     n <- fresh
-  --     -- iparam <- fresh
-  --     let v = Var n :: CExpression a
-  --         stmt = Bind (setup cf) n (Return v)
-  --     return $ Compiled { setup = stmt, result = Var n }
--- translate (NL.Fix f) = do
---   cf <- translate f
---   n <- fresh
---   -- iparam <- fresh
---   let v = Var n :: CExpression a
---       stmt = Bind (setup cf) n (Return v)
---   return $ Compiled { setup = stmt, result = Var n }
-
--- data EvalValue where
---   EvalValue :: Typeable a => CValue a -> EvalValue
-
--- valueToLiteral :: CValue a -> a
--- valueToLiteral (IntV i) = i
--- valueToLiteral (BoolV i) = i
--- valueToLiteral UnitV = ()
 
 unInt :: CValue Int -> Int
 unInt (IntV x) = x
@@ -248,23 +158,15 @@ evalExpr (Not x) m =
   let x' = evalExpr x m
   in BoolV (not (unBool x'))
 
--- type Eval a = State Int (EvalValue, Map Int EvalValue)
-
 evalStmt :: CStatement -> Env -> Env
--- evalStmt (Return (x :: CExpression a)) m = (Just (evalExpr x m), m)
 evalStmt (BindExpr x i y) m = 
   let v  = evalExpr x m
       m' = Extend i v m
   in evalStmt y m'
 evalStmt Skip m = m
-  -- let x' = evalExpr
-  -- case trace ">> bind x" $ evalStmt x m of
-  --     (Nothing, _) -> error $ "no value returned"
-      -- (Just (x' :: CValue a), m') -> evalStmt y (trace (">> bind extend v" ++ show i ++ " | type: " ++ show (typeRep x')) $ Extend i x' m')
-  -- in trace ">> bind y" $ evalStmt y m''
 evalStmt (Seq x y) m =
-  let m' = trace (">> seq x" ++ showCStmt 3 x m) $ evalStmt x m
-  in trace (">> seq y " ++ showCStmt 3 y m) $ evalStmt y m'
+  let m' = trace (">> seq x" ++ showCStmt 3 x) $ evalStmt x m
+  in trace (">> seq y " ++ showCStmt 3 y) $ evalStmt y m'
 evalStmt (If cond t e) m =
   let cond' = trace ">> if cond" $ evalExpr cond m
       condBool = unBool cond'
@@ -298,6 +200,9 @@ eval x arg m =
       FunV fn = evalExpr (result x) m'
   in fn arg
 
+indentStr :: Int -> String
+indentStr n = replicate (2 * n) ' '
+
 showBinOp :: AL.BinOp -> String
 showBinOp Plus  = "+"
 showBinOp Min   = "-"
@@ -317,49 +222,48 @@ showCValue UnitV = "null"
 showCValue (PairV x y) = "(" ++ showCValue x ++ ", " ++ showCValue y ++ ")"
 showCValue (FunV _) = error "cannot print function"
 
-showCStmt :: Int -> CStatement -> Env -> String
-showCStmt indent (UpdateVar i x) m = "\n" ++ indentStr indent ++ "v" ++ show i ++ " =~ " ++ showCExpression indent x m
-showCStmt indent (If cond t f) m =
-    "\n" ++ indentStr indent ++ "if " ++ showCExpression indent cond m ++ " {"
-    ++  showCStmt (indent + 1) t m
+showCStmt :: Int -> CStatement -> String
+showCStmt indent (UpdateVar i x) = "\n" ++ indentStr indent ++ "v" ++ show i ++ " =~ " ++ showCExpression x
+showCStmt indent (If cond t f) =
+    "\n" ++ indentStr indent ++ "if " ++ showCExpression cond ++ " {"
+    ++  showCStmt (indent + 1) t
     ++ "\n" ++ indentStr indent ++ "} else {"
-    ++ showCStmt (indent + 1) f m
+    ++ showCStmt (indent + 1) f
     ++ "\n" ++ indentStr indent ++ "}"
-showCStmt indent (While cond body) m =
-    "\n" ++ indentStr indent ++ "while " ++ showCExpression indent cond m ++ " {"
-    ++ showCStmt (indent + 1) body m
+showCStmt indent (While cond body) =
+    "\n" ++ indentStr indent ++ "while " ++ showCExpression cond ++ " {"
+    ++ showCStmt (indent + 1) body
     ++ "\n" ++ indentStr indent ++ "}"
-showCStmt indent (BindExpr x i y) m =
-    "\n" ++ indentStr indent ++ "let v" ++ show i ++ " = " ++ showCExpression (indent + 1) x m ++ " in"
-    ++ showCStmt (indent + 1) y m
-showCStmt indent (Seq x y) m =
-    showCStmt indent x m ++ showCStmt indent y m
-showCStmt indent (DefFun _ ifun iparam stup res) m =
+showCStmt indent (BindExpr x i y) =
+    "\n" ++ indentStr indent ++ "let v" ++ show i ++ " = " ++ showCExpression x ++ " in"
+    ++ showCStmt (indent + 1) y
+showCStmt indent (Seq x y) =
+    showCStmt indent x ++ showCStmt indent y
+showCStmt indent (DefFun _ ifun iparam stup res) =
     "\n" ++ indentStr indent ++ "function" ++ show ifun ++ " (v" ++ show iparam ++ ") {"
-    ++ showCStmt (indent + 1) stup m
-    ++ "\n" ++ indentStr (indent + 1) ++ "return " ++ showCExpression indent res m
+    ++ showCStmt (indent + 1) stup
+    ++ "\n" ++ indentStr (indent + 1) ++ "return " ++ showCExpression res
     ++ "\n" ++ indentStr indent ++ "}"
--- showCStmt indent (Return x) m =  "\n" ++ indentStr indent ++ "[Return " ++ showCExpression indent x m ++ "]"
-showCStmt indent (DefVar i f) m =  "\n" ++ indentStr indent ++ "v" ++ show i ++ " = " ++ showCExpression indent f m
-showCStmt _ Skip _ = ""
+showCStmt indent (DefVar i f) =  "\n" ++ indentStr indent ++ "v" ++ show i ++ " = " ++ showCExpression f
+showCStmt _ Skip = ""
 
-showCExpression :: Int -> CExpression a -> Env -> String
-showCExpression _ (Var i) _ = "v" ++ show i
-showCExpression indent (Not x) m = "!" ++ showCExpression indent x m
-showCExpression indent (LIntOp op x y) m = "(" ++ showCExpression indent x m ++ " " ++ showBinOp op ++ " " ++ showCExpression indent y m ++ ")"
-showCExpression indent (LCmpOp op x y) m = "(" ++ showCExpression indent x m ++ " " ++ showCmpOp op ++ " " ++ showCExpression indent y m ++ ")"
-showCExpression _ (Val v) _ = showCValue v
-showCExpression indent (CallExpr f arg) m = showCExpression indent f m ++ "(" ++ showCExpression indent arg m ++ ")"
-showCExpression indent (Prod l r) m = "(" ++ showCExpression indent l m ++ "," ++ showCExpression indent r m ++ ")"
-showCExpression indent (Fst p) m = showCExpression indent p m ++ "[0]"
-showCExpression indent (Snd p) m = showCExpression indent p m ++ "[1]"
+showCExpression :: CExpression a -> String
+showCExpression (Var i) = "v" ++ show i
+showCExpression (Not x) = "!" ++ showCExpression x
+showCExpression (LIntOp op x y) = "(" ++ showCExpression x ++ " " ++ showBinOp op ++ " " ++ showCExpression y ++ ")"
+showCExpression (LCmpOp op x y) = "(" ++ showCExpression x ++ " " ++ showCmpOp op ++ " " ++ showCExpression y ++ ")"
+showCExpression (Val v) = showCValue v
+showCExpression (CallExpr f arg) = showCExpression f ++ "(" ++ showCExpression arg ++ ")"
+showCExpression (Prod l r) = "(" ++ showCExpression l ++ "," ++ showCExpression r ++ ")"
+showCExpression (Fst p) = showCExpression p ++ "[0]"
+showCExpression (Snd p) = showCExpression p ++ "[1]"
 
 main :: IO ()
 main = do
     let (nl, c') = NL.translate 0 AL.fac
         cl = evalState (translate nl) c'
-        ev = eval cl (IntV 5) Empty
+        ev = eval cl (IntV 6) Empty
     putStrLn "--- Translating ---"
-    putStrLn $ showCStmt 0 (setup cl) Empty
+    putStrLn $ showCStmt 0 (setup cl)
     putStrLn "\n--- Evaluating ---\n"
     putStrLn $ showCValue ev
