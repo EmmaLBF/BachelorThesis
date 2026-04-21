4390ccd4d6652e08d404977580e2679bc497252d
EmmaLBF <em.lou.bilz.fer@gmail.com>
2026-04-21 18:54:50 +0200

    no compilation errors in the c file anymore

--- C.hs
+++ C.hs
@@ -29,7 +29,7 @@ data CStatement where
   Seq :: CStatement -> CStatement -> CStatement
   If :: CExpression Bool -> CStatement -> CStatement -> CStatement
   -- DefFun :: (Typeable a, Typeable b) => Proxy a -> Int -> Int -> CStatement -> CExpression b -> CStatement
-  DefFun    :: (Typeable b) 
+  DefFun    :: (Typeable b)
             => Proxy b      -- return type
             -> Int          -- function id
             -> CParams
@@ -46,7 +46,7 @@ translate (CL.BindExpr x i s) =
     let s' = translate s
     in BindExpr x i s'
 translate CL.Skip = Skip
-translate (CL.While cond x) = 
+translate (CL.While cond x) =
     let x' = translate x
     in While cond x'
 translate (CL.UpdateVar i x) = UpdateVar i x
@@ -65,6 +65,17 @@ translate (CL.Seq x y) =
 translate (CL.DefFun tret ifun (iparam, tparam) body ret) =
     let body' = translate body
     in DefFun tret ifun [CParam iparam tparam] body' ret
+
+-- takes list of free vars and cstmt
+lambdaLift :: CParams -> CStatement -> [CStatement]
+lambdaLift [] (DefFun tret ifun params body ret) = lambdaLift params body ++ [DefFun tret ifun params body ret]
+lambdaLift [CParam i t] (DefFun tret ifun params body ret) = lambdaLift (params ++ [CParam i t]) body ++ [DefFun tret ifun (params ++ [CParam i t]) body ret]
+lambdaLift l (Seq x y) =
+    let x' = lambdaLift l x
+        y' = lambdaLift l y
+    in x' ++ y'
+lambdaLift [] _ = []
+lambdaLift _ _ = []
 
 --   evalStmt (DefFunMultiArg (_ :: Proxy a) ifun iparams bodySetup (bodyResult :: CExpression b)) m =
 --   let fn = mkFn iparams m
@@ -106,6 +117,21 @@ showCParams [] = ""
 showCParams [CParam i t] = showProx (typeRep t) ++ " v" ++ show i
 showCParams (i:is) = showCParams [i] ++ ", " ++ showCParams is
 
+showCFunDecs :: [CStatement] -> String
+showCFunDecs [] = ""
+showCFunDecs [DefFun prox ifun params _ _] =
+    "\n" ++ showProx (typeRep prox) ++ " v" ++ show ifun ++ "(" ++ showCParams params ++ ");"
+showCFunDecs (i:is) = showCFunDecs[i] ++ showCFunDecs is
+
+showCFunDefs :: [CStatement] -> String
+showCFunDefs [] = ""
+showCFunDefs [DefFun prox ifun params body res] = 
+    "\n" ++ showProx (typeRep prox) ++ " v" ++ show ifun ++ "(" ++ showCParams params ++ ") {"
+    ++ showCStmt 1 body
+    ++ "\n" ++ "  return " ++ showCExpression res ++ ";"
+    ++ "\n}\n"
+showCFunDefs (i:is) = showCFunDefs[i] ++ showCFunDefs is
+
 showCStmt :: Int -> CStatement -> String
 showCStmt indent (UpdateVar i x) = "\n" ++ indentStr indent ++ "v" ++ show i ++ " = " ++ showCExpression x ++ ";"
 showCStmt indent (If cond t f) =
@@ -123,14 +149,14 @@ showCStmt indent (BindExpr x i y) =
     ++ showCStmt (indent + 1) y
 showCStmt indent (Seq x y) =
     showCStmt indent x ++ showCStmt indent y
-showCStmt indent (DefFun prox ifun params stup res) =
-    "\n" ++ indentStr indent ++ showProx (typeRep prox) ++ " v" ++ show ifun ++ "(" ++ showCParams params ++ ") {"
-    ++ showCStmt (indent + 1) stup
-    ++ "\n" ++ indentStr (indent + 1) ++ "return " ++ showCExpression res ++ ";"
-    ++ "\n" ++ indentStr indent ++ "}"
+-- showCStmt indent (DefFun prox ifun params stup res) =
+--     "\n" ++ indentStr indent ++ showProx (typeRep prox) ++ " v" ++ show ifun ++ "(" ++ showCParams params ++ ") {"
+--     ++ showCStmt (indent + 1) stup
+--     ++ "\n" ++ indentStr (indent + 1) ++ "return " ++ showCExpression res ++ ";"
+--     ++ "\n" ++ indentStr indent ++ "}"
 showCStmt indent (DefVar i f) =  "\n" ++ indentStr indent ++ showProx (typeRep f) ++ " v" ++ show i ++ " = " ++ showCExpression f ++ ";"
 showCStmt _ Skip = ""
-
+showCStmt _ _ = ""
 
 main :: IO ()
 main = do
@@ -140,12 +166,20 @@ main = do
     putStrLn "--- Translating to CLang ---"
     putStrLn $ CL.showCStmt 0 (CL.setup cl)
     putStrLn $ "return " ++ showCExpression (CL.result cl) ++ ";"
-    putStrLn "\n--- Translating to C ---"
+
+    putStrLn "\n--- Lambda Lift ---"
+    let defs = lambdaLift [] c
+    
+    putStrLn "\n--- Printing C ---"
     let fileName = "output.c"
-    let headers = "#include <stdbool.h>\n" ++ "typedef int (*intToint)(int);\n"
-    let body = showCStmt 0 c
-    let ret = "\nreturn " ++ CL.showCExpression (CL.result cl) ++ ";\n"
-    let content  = headers ++ body ++ ret
+    
+    let imports = "\n// imports\n#include <stdbool.h>\n"
+    let types = "\n// function ptr types\ntypedef int (*intToint)(int);\n"
+    let funDecs = "\n// function declarations" ++ showCFunDecs defs ++ "\n"
+    let funDefs = "\n// function definitions" ++ showCFunDefs defs
+    let body = "\nint main (void) {" ++ showCStmt 1 c
+    let ret = "\n  return " ++ CL.showCExpression (CL.result cl) ++ ";\n}"
+    let content  = imports ++ types ++ funDecs ++ funDefs ++ body ++ ret
     handle <- openFile fileName WriteMode
     hPutStrLn handle content
     hClose handle
--- output.c
+++ output.c
@@ -1,18 +1,29 @@
+// imports
 #include <stdbool.h>
+
+// function ptr types
 typedef int (*intToint)(int);
 
-intToint v4(intToint v0) {
-  int v3(int v1) {
-    int v2 = 1;
-    if (v1 == 0) {
-      v2 = 1;
-    } else {
-      v2 = (v1 * v0((v1 - 1)));
-    }
-    return v2;
+// function declarations
+int v3(int v1, intToint v0);
+intToint v4(intToint v0);
+
+// function definitions
+int v3(int v1, intToint v0) {
+  int v2 = 1;
+  if (v1 == 0) {
+    v2 = 1;
+  } else {
+    v2 = (v1 * v0((v1 - 1)));
   }
+  return v2;
+}
+
+intToint v4(intToint v0) {
   return v3;
 }
-intToint v5 = v4(v5);
-return v5;
 
+int main(void) {
+  intToint v5 = v4(v5);
+  return v5;
+}
